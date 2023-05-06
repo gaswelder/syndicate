434699b24eaa788dd21f6f41e999145482ec7e8d
gasweld <gasweld@gmail.com>
2023-05-06 04:06:00 +0300

    Fix logging

--- src/tracker.js
+++ src/tracker.js
@@ -115,12 +115,13 @@ const sendmail = async (config, subject, headers, html) => {
 const log = {
   error(msg) {
     process.stdout.write(
-      JSON.stringify({ level: "error", msg, t: new Date().toISOString() })
+      JSON.stringify({ level: "error", msg, t: new Date().toISOString() }) +
+        "\n"
     );
   },
   info(msg) {
     process.stdout.write(
-      JSON.stringify({ level: "info", msg, t: new Date().toISOString() })
+      JSON.stringify({ level: "info", msg, t: new Date().toISOString() }) + "\n"
     );
   },
 };
